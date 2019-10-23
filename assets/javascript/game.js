71b0ba7064b12ee188c59f85689cad748a0b1e82
Bradley Knutson <bradley@bknutson.com>
2019-10-22 21:18:16 -0600

    Added attacking function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,19 +4,28 @@ var starWars = {
     fighters: {
         jarJarBinks: {
             health: 250,
-            attack: 10,
+            attack: 5,
+            baseAttack: 5,
             counterAttack: 20,
         },
         generalKenobi: {
             health: 150,
-            attack: 15,
+            attack: 10,
+            baseAttack: 10,
             counterAttack: 30
         },
         anakin: {
             health: 200,
-            attack: 12,
+            attack: 7,
+            baseAttack: 7,
             counterAttack: 25
         }
+    },
+    gameStart: function(){
+        $('#attack').on('click', function(){
+            starWars.attack();
+            console.log(starWars);
+        });
     },
     chooseFighter: function(){
         if(starWars.chosenPlayer === ''){
@@ -33,8 +42,16 @@ var starWars = {
                 }
             });
         }
+    },
+    attack: function(){
+        if(this.chosenEnemy){
+            this.fighters[this.chosenPlayer].health -= this.fighters[this.chosenEnemy].counterAttack;
+            this.fighters[this.chosenEnemy].health -= this.fighters[this.chosenPlayer].attack;
+            this.fighters[this.chosenPlayer].attack += this.fighters[this.chosenPlayer].baseAttack;
+        }
     }
 }
 
 
+starWars.gameStart();
 starWars.chooseFighter();
